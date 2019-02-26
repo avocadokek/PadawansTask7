606b3bcd837efafeea421dca649bcf281f511b5c
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-26 13:28:57 +0200

    Add files via upload

--- StringExtension.cs
+++ StringExtension.cs
@@ -1,13 +1,48 @@
 ﻿using System;
+using System.Text;
 
 namespace PadawansTask7
 {
     public class StringExtension
     {
-        public static void OrderStringsByLength(string[] array)
+        public static string OrderStringsByLength(string[] array)
         {
-            // put your code here
+
+            for (int i = 1; i < array.Length; i++)
+            {
+               
+               
+                    int j = i - 1;
+
+                    string temp = array[i];
+
+                    while (j >= 0 && temp.Length < array[j].Length)
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                    }
+                    array[j + 1] = temp;
+
+                
+            }
+
+           if(array==null)
+            {
+                throw new ArgumentNullException();
+            }
+           if (array.GetType()!=typeof(string[]))
+            {
+                throw new ArgumentException();
+            }
+
+            return null;
+
+
+
             throw new NotImplementedException();
+            
+            throw new Exception();
+
         }
     }
 }
